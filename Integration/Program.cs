53380a5f0b65a41ea6fab2cc47edc857797b8e83
berkayopak <berkayopak@gmail.com>
2023-12-08 01:09:53 +0300

    Other methods we can apply for thread management have been added as comments

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -35,6 +35,21 @@ public abstract class Program
         }
         else
         {
+            /*
+            //We can use Parallel for waiting all actions. Blocking IO
+            Parallel.Invoke(
+                () => service.SaveItem("a"),
+                () => service.SaveItem("b"),
+                () => service.SaveItem("c"));
+
+            //Or we can use tasks with WaitAll too for waiting all actions. Blocking IO
+            Task task1 = Task.Factory.StartNew(() => service.SaveItem("a"));
+            Task task2 = Task.Factory.StartNew(() => service.SaveItem("b"));
+            Task task3 = Task.Factory.StartNew(() => service.SaveItem("c"));
+            Task.WaitAll(task1, task2, task3);
+            */
+
+            //This actions will run in parallel too, but we're not waiting in here. Non-Blocking IO
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
@@ -44,8 +59,16 @@ public abstract class Program
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
             ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+
+            //https://learn.microsoft.com/tr-tr/dotnet/api/system.threading.threadpool.queueuserworkitem?view=net-8.0
+            //P.S: You can add ManualResetEvent to your actions and you can call below function for wait all events
+            //WaitHandle.WaitAll(ManualResetEventArray);
         }
 
+        //P.S: Since we use Thread.Sleep, we create a blocking IO situation,
+        //but since we do not have a system that can run continuously,
+        //we keep it waiting in this way. We could have used the other
+        //blocking IO methods(Task, Parallel) shown above instead of this approach.
         Thread.Sleep(5000);
 
         Console.WriteLine("Everything recorded:");
